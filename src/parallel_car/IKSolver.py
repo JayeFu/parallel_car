a8b31e77898bf0fae1ebfd8d3b01286bb06f14a1
Jiawei Fu <fjw16@mails.tsinghua.edu.cn>
2020-04-16 16:57:58 +0800

    add matrix tf

--- src/parallel_car/IKSolver.py
+++ src/parallel_car/IKSolver.py
@@ -7,8 +7,10 @@ import tf2_ros
 import geometry_msgs.msg
 import copy
 
+import numpy as np
+
 class ParallelPose:
-    """A class to contain the pose of the parallel car
+    """A class to contain the pose of the parallel car, given the pose of wx_link
     """
     def __init__(self, x=0.0, y=0.0, theta=0.0, alpha=0.0):
         """Constructor for ParallelPose
@@ -82,6 +84,9 @@ class ParallelIKSolver:
 
         # a list of transfromation from down_link to down_i (i from 1 to 6), fixed tf, no change afterwards
         self._down_to_down_num_tf_list = list()
+        # a list of 
+        self._T_down_to_down_num_list = list()
+
         # a list of transfromation from up_link to up_i (i from 1 to 6), fixed tf, no change afterwards
         self._up_to_up_num_tf_list = list()
 
@@ -137,10 +142,12 @@ class ParallelIKSolver:
             self._up_to_up_num_tf_list = list()
             return False
 
+
         return True
 
     def calculate_pole_length_from_target(self, parallel_pose_desired):
         pole_length_list = list()
+
 
 
 
@@ -158,8 +165,54 @@ class ParallelIKSolver:
         """
         return copy.copy(self._pole_length_list)
 
-    def quaternion_to_rotation_matrix(self, rot):
-        pass
+    def quaternion_to_rotation_matrix(self, quat):
+        """A function to transform quaternion to homogeneous rotation matrix
+        
+        Arguments:
+            rot {Quaternion} -- Quaternion-type msg
+        
+        Returns:
+            Numpy matrix in 4x4  -- Result homogeneous rotation matrix from input quaternion
+        """
+        # get qx, qy, qz and qw from Quaternion-type msg
+        qx = quat.x
+        qy = quat.y
+        qz = quat.z
+        qw = quat.w
 
-    def vector3_to_translation_matrix(self, trans):
-        pass
+        rot_matrix = np.mat(np.eye(4))
+
+        rot_matrix[0, 0] = 1-2*np.square(qy)-2*np.square(qz)
+        rot_matrix[0, 1] = 2*qx*qy-2*qz*qw
+        rot_matrix[0, 2] = 2*qx*qz+2*qy*qw
+        rot_matrix[1, 0] = 2*qx*qy+2*qz*qw
+        rot_matrix[1, 1] = 1-2*np.square(qx)-2*np.square(qz)
+        rot_matrix[1, 2] = 2*qy*qz-2*qx*qw
+        rot_matrix[2, 0] = 2*qx*qz-2*qy*qw
+        rot_matrix[2, 1] = 2*qy*qz+2*qx*qw
+        rot_matrix[2, 2] = 1-2*np.square(qx)-2*np.square(qy)
+        
+        return rot_matrix
+
+
+    def vector3_to_translation_matrix(self, vec3):
+        """A function to transfrom from Vector3-type msg to homogeneous translation matrix
+        
+        Arguments:
+            vec3 {Vector3} -- Vector3-type msg
+        
+        Returns:
+            Numpy matrix in 4x4  -- Result homogeneous translation matrix from input vector3
+        """
+        
+        # get x, y and z from Vector3-type msg
+        x = vec3.x
+        y = vec3.y
+        z = vec3.z
+
+        trans_matrix = np.mat(np.eye(4))
+        trans_matrix[0, 3] = x
+        trans_matrix[1, 3] = y
+        trans_matrix[2, 3] = z
+
+        return trans_matrix
